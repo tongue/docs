aa2403ad74c1188c8f4e362714512291309a88f3
Johan Bergström <johan.bergstrom@isotop.se>
2023-04-24 15:43:04 +0200

    persist theme if switched

--- src/lib/components/theme-switcher.svelte
+++ src/lib/components/theme-switcher.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-	let theme = matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+	import { onMount } from 'svelte';
 
-	const hande_theme_switch = (new_theme: 'dark' | 'light') => () => {
+	const STORAGE_KEY = 'docs-theme';
+
+	let theme: 'dark' | 'light' = matchMedia('(prefers-color-scheme: dark)').matches
+		? 'dark'
+		: 'light';
+
+	const handle_theme_switch = (new_theme: 'dark' | 'light') => () => {
 		if (new_theme === 'dark') {
 			document.body.classList.remove('light');
 			document.body.classList.add('dark');
@@ -9,13 +15,20 @@
 			document.body.classList.remove('dark');
 			document.body.classList.add('light');
 		}
-
+		localStorage.setItem(STORAGE_KEY, new_theme);
 		theme = new_theme;
 	};
+
+	onMount(() => {
+		const persisted_theme = localStorage.getItem(STORAGE_KEY);
+		if ((persisted_theme === 'dark' || persisted_theme === 'light') && persisted_theme !== theme) {
+			handle_theme_switch(persisted_theme)();
+		}
+	});
 </script>
 
 {#if theme === 'dark'}
-	<button on:click={hande_theme_switch('light')}>Switch to Light theme</button>
+	<button on:click={handle_theme_switch('light')}>Switch to Light theme</button>
 {:else}
-	<button on:click={hande_theme_switch('dark')}>Switch to Dark theme</button>
+	<button on:click={handle_theme_switch('dark')}>Switch to Dark theme</button>
 {/if}
