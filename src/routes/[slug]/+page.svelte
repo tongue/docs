22d3e36ec7c86f160a187d3b20d76e7e3a7ee347
Johan Bergström <johan.bergstrom@alster.se>
2023-04-27 01:05:52 +0200

    refactor page component to +page route

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,9 +1,80 @@
 <script lang="ts">
-	import Page from "$lib/components/page.svelte";
+	import { active_section } from "$lib/actions/active-section";
+	import NavigationList from "$lib/components/navigation-list.svelte";
+	import PageHeader from "$lib/components/page-header.svelte";
 	import '$lib/styles/code.css';
 
 	export let data;
 	const { title, slug, sections, modified, edit, author } = data;
+
+	let active: string | null = null;
+
+	const on_active = (event: CustomEvent<string>) => {
+		active = event.detail;
+	};
 </script>
 
-<Page {title} {slug} {sections} {modified} {edit} {author} />
+<svelte:head>
+	<title>{title} | Alster Docs</title>
+</svelte:head>
+<article use:active_section on:activesection={on_active}>
+	<div class="content">
+		<PageHeader {slug} {author} {modified} {edit}>{title}</PageHeader>
+		{#each sections as section}
+			<section id={section.slug}>
+				{#if section.title && section.slug}
+					<h2 id={section.slug}><a href={`#${section.slug}`}>{section.title}</a></h2>
+				{/if}
+				{@html section.content}
+			</section>
+		{/each}
+	</div>
+	{#if sections.length > 1}
+		<aside>
+			<nav>
+				<h2>In this article</h2>
+				<NavigationList tag="ol">
+					{#each sections as section}
+						{#if section.title && section.slug}
+							<li>
+								<a
+									href={`#${section.slug}`}
+									aria-current={active === section.slug ? 'location' : undefined}>{section.title}</a
+								>
+							</li>
+						{/if}
+					{/each}
+				</NavigationList>
+			</nav>
+		</aside>
+	{/if}
+</article>
+
+<style>
+	aside {
+		display: none;
+	}
+
+	@media (min-width: 73rem) {
+		aside {
+			display: block;
+		}
+
+		article {
+			position: relative;
+			display: grid;
+			gap: 0 var(--gap-width);
+			grid-template-columns: auto var(--aside-width);
+		}
+	}
+
+	nav {
+		position: sticky;
+		top: calc(var(--header-height) + var(--top-gutter));
+	}
+
+	nav > h2 {
+		font-size: 1rem;
+		margin-bottom: 1.5rem;
+	}
+</style>
