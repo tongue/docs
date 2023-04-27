c06416da852b753bf283264b7733cc039afe1be8
Johan Bergström <johan.bergstrom@alster.se>
2023-04-28 00:02:00 +0200

    fix where to read  from

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import { dev } from '$app/environment';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
